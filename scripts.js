54bae6f53a1bcbcb20964f57350806b4aa6a909d
sivamshgss <101281718+sivamshgss@users.noreply.github.com>
2025-02-16 15:16:38 +0530

    Complete getComputerChoice function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello, World");
+function getComputerChoice() {
+    let choice = Math.random();
+    if (choice < (1 / 3)) {
+        choice = "Rock";
+    } else if (choice < (2 / 3)) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    return choice;
+}
